9ccbf4229edfd4a567c55c5a4d2cfe0abcba595a
Hanna <hanna@MacBook-Air-Hanna.local>
2021-08-23 21:31:57 +0300

    add require

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-console.log(6)
-console.log(process.argv[2]) // достать аргумент
+const main = require('./main.js')
+// const a = require('./main.js')
+// const b = require('./main.js')
+
+// console.log(main.Apple.getColor())
+
+const apple = new main.Apple()
+console.log(apple.getColor())
+
+// console.log(main, a, b)
+
+// console.log(6)
+// console.log(process.argv[2]) // достать аргумент
 
